7da356ae202316c9f738684cd3b632aefcedd0ec
ChanwheKim <chanwhe.kim@gmail.com>
2018-09-04 20:50:34 +0900

    Add calendar.js file to start making the calendar

---
+++ calendar.js
@@ -0,0 +1,20 @@
+var UIcontroller = (function() {
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    return {
+
+        displayDates: function() {
+
+            
+        }
+    }
+
+})();
+
+
+var calController = (function(UIctrl) {
+
+
+
+})(UIcontroller);
