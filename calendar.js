6da1d0960e403c50d1f1ee70af8ebe6292fe9b5c
ChanwheKim <chanwhe.kim@gmail.com>
2018-09-09 00:04:15 +0900

    Add calculation to get first visible date of prev calendar

--- calendar.js
+++ calendar.js
@@ -5,7 +5,7 @@ var UIcontroller = (function() {
     return {
 
         displayDates: function() {
-            var firstDay, numOfDays, date;
+            var firstDay, numOfDays, date, datePrevMonth;
 
             // 1. Determine 1st day of the month
             firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
@@ -15,6 +15,9 @@ var UIcontroller = (function() {
 
             // 3. Initial Date Value
             date = 1;
+
+            // 4. Set up date of previous month
+            datePrevMonth = -(6 - (7 - firstDay));
 
             
         }
